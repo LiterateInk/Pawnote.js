3ded5897d60efa04b97e07dda7ef15e34959cbd1
Gabriel29306 <ggkervran@outlook.com>
2025-01-10 23:03:09 +0100

    fix(grades): use new type of Pronote grades

--- src/decoders/subject-averages.ts
+++ src/decoders/subject-averages.ts
@@ -7,9 +7,9 @@ export const decodeSubjectAverages = (average: any): SubjectAverages => {
     student: average.moyEleve && decodeGradeValue(average.moyEleve.V),
     outOf: average.baremeMoyEleve && decodeGradeValue(average.baremeMoyEleve.V),
     defaultOutOf: average.baremeMoyEleveParDefaut && decodeGradeValue(average.baremeMoyEleveParDefaut.V),
-    class_average: decodeGradeValue(average.moyClasse.V),
-    min: decodeGradeValue(average.moyMin.V),
-    max: decodeGradeValue(average.moyMax.V),
+    class_average: average.moyClasse && decodeGradeValue(average.moyClasse.V),
+    min: average.moyMin && decodeGradeValue(average.moyMin.V),
+    max: average.moyMax && decodeGradeValue(average.moyMax.V),
     subject: decodeSubject(average),
     backgroundColor: average.couleur
   };
