d04113508b7e169faae8d21400acd21650d6f869
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-11 01:30:20 +0100

    fix(presence): use Navigation to replicate old Presence behavior on latest update

--- src/api/presence.ts
+++ src/api/presence.ts
@@ -1,11 +1,30 @@
 import { RequestFN } from "~/core/request-function";
 import { TabLocation, type SessionHandle } from "~/models";
 import { apiProperties } from "./private/api-properties";
+import { isVersionGte2024_3_9 } from "./private/versions";
 
 export const presence = async (session: SessionHandle): Promise<void> => {
-  const request = new RequestFN(session, "Presence", {
-    [apiProperties(session).signature]: { onglet: TabLocation.Presence }
-  });
+  const properties = apiProperties(session);
+  let request: RequestFN;
+
+  // Since v2024.3.9, we need to replicate the
+  // old behavior of `Presence` request.
+  if (isVersionGte2024_3_9(session.instance.version)) {
+    request = new RequestFN(session, "Navigation", {
+      [properties.signature]: { onglet: TabLocation.Presence },
+      [properties.data]: {
+        onglet: TabLocation.Presence,
+        ongletPrec: TabLocation.Presence
+      }
+    });
+  }
+  // Before v2024.3.9, we can directly use `Presence`
+  // request without any issue.
+  else {
+    request = new RequestFN(session, "Presence", {
+      [properties.signature]: { onglet: TabLocation.Presence }
+    });
+  }
 
   await request.send();
 };
