f61b5cc1dff33b5631b6416685c3fb4521582585
Gabriel29306 <ggkervran@outlook.com>
2025-01-17 20:53:19 +0100

    feat(studentGradebook): add model
    Signed-off-by: Gabriel29306 <ggkervran@outlook.com>

---
+++ src/models/gradebook.ts
@@ -0,0 +1,41 @@
+import { Subject } from "./subject";
+
+export type GradeBook =
+  | GradeBookNot
+  | GradeBookOk;
+
+type GradeBookNot = Readonly<{
+  available: false;
+  message?: string;
+}>;
+
+type GradeBookOk = Readonly<{
+  available: true;
+  /**
+   * The differents assessments like the mentions or the overall rating
+   */
+  overallAssessments: {
+    name: string;
+    value: string;
+  }[];
+  /**
+   * graph image as base64 png (with white backgound)
+   */
+  graph?: string;
+  subjects: GradeBookSubject[];
+  url: string;
+}>;
+
+export type GradeBookSubject = {
+  subject: Subject;
+  subjectGroup: string;
+  coef: number;
+  averages: {
+    student: number;
+    classOverall: number;
+    max: number;
+    min: number;
+  };
+  assessments: string[];
+  teachers: string[];
+};
