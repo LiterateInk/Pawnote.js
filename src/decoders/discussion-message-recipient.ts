2a0910ffc184617e8b8cf3af6b0aaa2f22c1515b
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-05-02 15:28:38 +0200

    fix: remove useless function #75

--- src/decoders/discussion-message-recipient.ts
+++
@@ -1,8 +0,0 @@
-import type { DiscussionMessageRecipient } from "~/models/discussion-message-recipient";
-
-export const decodeDisccusionMessageRecipient = (recipient: any): DiscussionMessageRecipient => {
-  return {
-    name: recipient.L,
-    kind: recipient.G
-  };
-};
