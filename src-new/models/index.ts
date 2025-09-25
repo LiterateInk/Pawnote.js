07ed2b091d4267082a50befbb5134089f7233f54
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-09-25 13:07:31 +0200

    chore: exports

---
+++ src-new/models/index.ts
@@ -0,0 +1,5 @@
+export { Instance } from "./Instance";
+export { InstanceInformation } from "./InstanceInformation";
+export { Login } from "./Login";
+export { Session } from "./Session";
+export { Webspace } from "./Webspace";
