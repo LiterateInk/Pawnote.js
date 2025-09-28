2ddc3ea42454e0d8674fb7a51c657645afe6a8e6
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-09-29 01:43:30 +0200

    feat(api): add chaine brute and html safe http variables deserializers

---
+++ src-new/api/HttpVariables/TypeHttpHtmlSafe.ts
@@ -0,0 +1,15 @@
+import { TypeHttpVariable } from "./TypeHttpVariable";
+
+interface HtmlSafe {
+  _T: TypeHttpVariable.TypeHttpHtmlSafe
+  V: string
+}
+
+export class TypeHttpHtmlSafe {
+  public static deserializer = (value: HtmlSafe): string => {
+    if (value._T !== TypeHttpVariable.TypeHttpHtmlSafe)
+      throw new Error("HTTP type is not compatible");
+
+    return value.V;
+  };
+}
