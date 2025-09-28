fc58ae3bc38f1519be63dcd438d281d998293c3d
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-09-28 05:30:49 +0200

    feat(compress): add inflate and deflate optimized helpers
    uses node:zlib on node.js, Bun methods on Bun and fflate on other platforms

---
+++ src-new/core/deflate.ts
@@ -0,0 +1,14 @@
+export const deflate = async (input: Uint8Array): Promise<Uint8Array> => {
+  if (typeof process !== "undefined") {
+    if (process.versions.bun) {
+      return Bun.deflateSync(input, { library: "libdeflate" });
+    }
+    else if (process.versions.node) {
+      const { deflateRawSync } = await import("node:zlib");
+      return new Uint8Array(deflateRawSync(input));
+    }
+  }
+
+  const { deflateSync } = await import("fflate");
+  return deflateSync(input);
+};
---
+++ src-new/core/inflate.ts
@@ -0,0 +1,14 @@
+export const inflate = async (input: Uint8Array): Promise<Uint8Array> => {
+  if (typeof process !== "undefined") {
+    if (process.versions.bun) {
+      return Bun.inflateSync(input, { library: "libdeflate" });
+    }
+    else if (process.versions.node) {
+      const { inflateRawSync } = await import("node:zlib");
+      return new Uint8Array(inflateRawSync(input));
+    }
+  }
+
+  const { inflateSync } = await import("fflate");
+  return inflateSync(input);
+};
