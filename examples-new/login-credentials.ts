f60edd0916bf4259b496c43482b499577ec52cdb
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-21 00:48:04 +0200

    docs: rewrite login example

--- examples-new/login-credentials.ts
+++ examples-new/login-credentials.ts
@@ -1,7 +1,12 @@
-import { Instance, StudentLogin, Webspace } from "../src-new";
+import { Instance, StudentLogin } from "../src-new";
 
-const instance = Instance.fromURL("https://demo.index-education.net/pronote/eleve.html");
+const instance = Instance.fromURL("http://pronote-vm.dev/pronote/eleve.html");
 const login = new StudentLogin(instance);
-await login.initializeWithCredentials("demonstration", "pronotevs");
-// console.log(login.requiresMFA);
-// const student = login.finalize().toStudent();
+await login.initializeWithCredentials(Bun.env.USERNAME!, Bun.env.PASSWORD!);
+
+if (login.requires2FA) {
+  console.log("TODO: 2FA is required!");
+}
+
+// const student = await login.finalize();
+// await student.timetable();
