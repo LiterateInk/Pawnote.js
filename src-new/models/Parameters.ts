e7a655a3d585dd1e15d6515f2d1cc9184b7a939b
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-21 18:17:03 +0200

    feat(params): export navigator identifier

--- src-new/models/Parameters.ts
+++ src-new/models/Parameters.ts
@@ -4,4 +4,8 @@ export class Parameters {
   public constructor(
     private readonly parameters: FonctionParametresResponse
   ) {}
+
+  public get navigatorIdentifier(): string {
+    return this.parameters.data.navigatorIdentifier;
+  }
 }
