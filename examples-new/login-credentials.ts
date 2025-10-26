b48e1b9fc7791755b39e013e5ff3792000c62a69
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-26 18:15:12 +0100

    docs: add more to the login credentials example

--- examples-new/login-credentials.ts
+++ examples-new/login-credentials.ts
@@ -1,12 +1,76 @@
-import { Instance, StudentLogin } from "../src-new";
+import { Instance, StudentLoginPortal } from "../src-new";
 
 const instance = Instance.fromURL(Bun.env.URL!);
-const login = new StudentLogin(instance);
-await login.initializeWithCredentials(Bun.env.USERNAME!, Bun.env.PASSWORD!);
+const portal = new StudentLoginPortal(instance);
+const auth = await portal.credentials(Bun.env.USERNAME!, Bun.env.PASSWORD!);
 
-if (login.requires2FA) {
-  console.log("TODO: 2FA is required!");
+console.log(`authenticating to ${instance.base}...`);
+
+// On the first ever login, you might be asked to custom your password.
+// Also, administrators can reset your password and you will also be
+// asked to custom your password on the next login.
+if (auth.shouldCustomPassword) {
+  console.log("you have to custom the password, make sure to respect the following rules.");
+  console.log("\t- max:", auth.password.max);
+  console.log("\t- min:", auth.password.min);
+
+  if (auth.password.withAtLeastOneLetter)
+    console.log("\t- with at least one letter");
+
+  if (auth.password.withAtLeastOneNumericCharacter)
+    console.log("\t- with at least one numeric character");
+
+  if (auth.password.withAtLeastOneSpecialCharacter)
+    console.log("\t- with at least one special character");
+
+  if (auth.password.withLowerAndUpperCaseMixed)
+    console.log("\t- with lower and upper case mixed");
+
+  while (true) {
+    const password = prompt("new password>");
+
+    if (!password) continue;
+    if (await auth.validate(password)) break;
+    else console.warn("[!] this password is not strong enough, please respect all the rules");
+  }
 }
 
-// const student = await login.finalize();
+if (auth.shouldCustomDoubleAuth) {
+  console.log("CUSTOM MFA");
+  // if (auth.modes.includes(DoubleAuthMode.PIN)) {
+  //   auth.mode.pin("1234");
+  // }
+  // else {
+  //   auth.mode.nothing();
+  // }
+}
+
+if (auth.shouldEnterPIN) {
+  while (true) {
+    const pin = prompt("pin>");
+
+    if (!pin) continue;
+    if (await auth.verify(pin)) break;
+    else console.warn("[!] incorrect pin, try again");
+  }
+}
+
+if (auth.shouldRegisterSource) {
+  while (true) {
+    const device = prompt("device>", `Pawnote.js/${Date.now()}`);
+
+    if (!device) continue;
+    if (device.length > 30) {
+      console.warn("[!] device name should be <=30 characters length");
+      continue;
+    }
+
+    const alreadyKnown = await auth.source(device);
+    if (alreadyKnown) console.info("[*] this device is already known, registration will be skipped");
+    break;
+  }
+}
+
+const student = await portal.finish(auth);
 // await student.timetable();
+// console.log(student);
