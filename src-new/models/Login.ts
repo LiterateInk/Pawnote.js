ba2defa425d151b9474074ab03f4f1d5f51f9ba9
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-26 16:00:57 +0100

    feat(api): add double auth methods to check source, pin, password rules and finalize auth

--- src-new/models/Login.ts
+++ src-new/models/Login.ts
@@ -16,6 +16,10 @@ import { Identity } from "./Identity";
 import { Authentication } from "./Authentication";
 import { Student } from "./Student";
 import { TypeActionIHMSecurisationCompte } from "../api/models/TypeActionIHMSecurisationCompte";
+import { PasswordRules } from "./PasswordRules";
+import { SecurisationCompteDoubleAuth } from "src-new/api/SecurisationCompteDoubleAuth";
+import { SourceTooLongError } from "./Errors/SourceTooLongError";
+import { ParametresUtilisateur } from "src-new/api/ParametresUtilisateur";
 
 /**
  * An intermediate class where a user is half authenticated.
@@ -23,7 +27,7 @@ import { TypeActionIHMSecurisationCompte } from "../api/models/TypeActionIHMSecu
  * Login portal might wait for additional data
  * such as PIN code or password change for example.
  */
-class PendingLogin {
+export class PendingLogin {
   /** @internal */
   public constructor(
     /** @internal */
@@ -49,10 +53,77 @@ class PendingLogin {
       TypeActionIHMSecurisationCompte.AIHMSC_SaisieCodePINetSource
     );
   }
-  public get shouldRegisterDevice(): boolean {
+  public get shouldRegisterSource(): boolean {
     return this._authentication.securityActions.includes(
       TypeActionIHMSecurisationCompte.AIHMSC_SaisieSourcePourNotifSeulement
     );
+  }
+
+  /**
+   * If you have to custom the password, this property
+   * gives you all the rules you have to respect in
+   * order to create a new password.
+   */
+  public get password(): PasswordRules {
+    return this._authentication.password;
+  }
+
+  /** @internal */
+  public _password?: string;
+
+  /**
+   * Checks a given password against the password
+   * rules defined in the server configuration.
+   *
+   * Will let you know if you can change your password
+   * to this or not.
+   *
+   * @param password - the password to check against the server rules
+   * @returns whether the given password is allowed or not
+   */
+  public async validate(password: string): Promise<boolean> {
+    const ok = await new SecurisationCompteDoubleAuth(this._session)
+      .sendPasswordCheck(password);
+
+    if (ok) this._password = password;
+    return ok;
+  }
+
+  /** @internal */
+  public _pin?: string;
+
+  /**
+   * Verify a given PIN code.
+   *
+   * @param pin - the pin code to check
+   * @returns whether the pin code is correct or not
+   */
+  public async verify(pin: string): Promise<boolean> {
+    const ok = await new SecurisationCompteDoubleAuth(this._session)
+      .sendPinVerify(pin);
+
+    if (ok) this._pin = pin;
+    return ok;
+  }
+
+  /** @internal */
+  public _source?: string;
+
+  /**
+   * Checks a given source to know if it is already known.
+   * If yes, then it'll skip the registration at save time.
+   *
+   * @param source - the source to check, should be <=30 characters
+   * @returns whether the source is already known
+   */
+  public async source(source: string): Promise<boolean> {
+    if (source.length > 30)
+      throw new SourceTooLongError(30);
+
+    this._source = source;
+
+    return new SecurisationCompteDoubleAuth(this._session)
+      .sendSourceAlreadyKnown(source);
   }
 }
 
@@ -85,7 +156,8 @@ abstract class LoginPortal {
     const challenge = identity.solveChallenge(session, key);
 
     const authentication = new Authentication(
-      await new Authentification(session).send(challenge)
+      await new Authentification(session).send(challenge),
+      identity.username ?? username
     );
 
     authentication.switchDefinitiveKey(session, key);
@@ -133,7 +205,8 @@ abstract class LoginPortal {
     const challenge = identity.solveChallenge(session, key);
 
     const authentication = new Authentication(
-      await new Authentification(session).send(challenge)
+      await new Authentification(session).send(challenge),
+      identity.username ?? username
     );
 
     authentication.switchDefinitiveKey(session, key);
@@ -146,11 +219,17 @@ abstract class LoginPortal {
   }
 
   /**
-   * Sends the final payload.
-   * @returns user parameters for the logged in user
+   * @returns user parameters
    */
   protected async _finish(login: PendingLogin) {
-    return "ok";
+    if (login.shouldCustomDoubleAuth || login.shouldCustomPassword || login.shouldEnterPIN || login.shouldRegisterSource) {
+      const token = await new SecurisationCompteDoubleAuth(login._session)
+        .save(void 0, login._password, login._pin, login._source);
+
+      if (token) login._authentication.token = token;
+    }
+
+    return new ParametresUtilisateur(login._session).send();
   }
 
   private async _getWebspaceHomepageSession(webspace: Webspace, cookies: Record<string, string> = {}): Promise<HomepageSession> {
