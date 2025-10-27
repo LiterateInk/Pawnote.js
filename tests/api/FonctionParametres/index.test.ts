fc3e7bde394339a5702cf4b7f76ccf8d9741714f
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-27 23:10:55 +0100

    chore: add wip tests data for parents

--- tests/api/FonctionParametres/index.test.ts
+++ tests/api/FonctionParametres/index.test.ts
@@ -1,11 +1,18 @@
 import students_1 from "./students_1.json";
-import { describe, it } from "bun:test";
+import parents_1 from "./parents_1.json";
+
 import { FonctionParametresModel } from "../../../src-new/api/FonctionParametres/response";
+import { describe, it } from "bun:test";
 import { deserialize } from "desero";
 
 describe("FonctionParametres", () => {
   it("should decode [students]", () => {
     const parameters = deserialize(FonctionParametresModel, students_1.data);
+    // console.log(parameters);
+  });
+
+  it("should decode [parents]", () => {
+    const parameters = deserialize(FonctionParametresModel, parents_1.data);
     console.log(parameters);
   });
 });
--- tests/api/ParametresUtilisateur/index.test.ts
+++ tests/api/ParametresUtilisateur/index.test.ts
@@ -1,11 +1,18 @@
 import students_1 from "./students_1.json";
-import { describe, it } from "bun:test";
+import parents_1 from "./parents_1.json";
+
 import { ParametresUtilisateurModel } from "../../../src-new/api/ParametresUtilisateur/response";
+import { describe, it } from "bun:test";
 import { deserialize } from "desero";
 
 describe("ParametresUtilisateur", () => {
   it("should decode [students]", () => {
     const parameters = deserialize(ParametresUtilisateurModel, students_1.data);
+    // console.dir(parameters, { depth: Infinity });
+  });
+
+  it("should decode [parents]", () => {
+    const parameters = deserialize(ParametresUtilisateurModel, parents_1.data);
     console.dir(parameters, { depth: Infinity });
   });
 });
