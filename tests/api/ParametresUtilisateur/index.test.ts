65ab9dca0cc6b47df11b4faa448e142e03190f8a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-28 00:25:45 +0100

    chore: add more parents testing data

--- tests/api/ParametresUtilisateur/index.test.ts
+++ tests/api/ParametresUtilisateur/index.test.ts
@@ -1,5 +1,6 @@
 import students_1 from "./students_1.json";
 import parents_1 from "./parents_1.json";
+import parents_2 from "./parents_2.json";
 
 import { ParametresUtilisateurModel } from "../../../src-new/api/ParametresUtilisateur/response";
 import { describe, it } from "bun:test";
@@ -11,8 +12,13 @@ describe("ParametresUtilisateur", () => {
     // console.dir(parameters, { depth: Infinity });
   });
 
-  it("should decode [parents]", () => {
+  it("should decode [parents 1]", () => {
     const parameters = deserialize(ParametresUtilisateurModel, parents_1.data);
+    // console.dir(parameters, { depth: Infinity });
+  });
+
+  it("should decode [parents 2]", () => {
+    const parameters = deserialize(ParametresUtilisateurModel, parents_2.data);
     console.dir(parameters, { depth: Infinity });
   });
 });
