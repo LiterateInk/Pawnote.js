a63dbcb91e590c8f2ecdf2988955154e5c0b09ca
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-07 16:07:03 +0100

    chore: remove old

--- src/encoders/user-resource.ts
+++
@@ -1,8 +0,0 @@
-import type { UserResource } from "~/models";
-
-// we simply encode the entity, nothing more is required most of the time.
-export const encodeUserResource = (resource: UserResource): any => ({
-  G: resource.kind,
-  L: resource.name,
-  N: resource.id
-});
