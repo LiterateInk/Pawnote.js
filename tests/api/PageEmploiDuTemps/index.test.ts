df9718e146ae4d3c46fa65d5618cbcc9873b1951
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-08 18:32:16 +0100

    chore: add tests data for timetable

---
+++ tests/api/PageEmploiDuTemps/index.test.ts
@@ -0,0 +1,12 @@
+import parents_1 from "./parents_1.json";
+
+import { PageEmploiDuTempsModel } from "../../../src-new/api/PageEmploiDuTemps/response";
+import { describe, it } from "bun:test";
+import { deserialize } from "desero";
+
+describe("PageEmploiDuTemps", () => {
+  it("should decode [parents]", () => {
+    const parameters = deserialize(PageEmploiDuTempsModel, parents_1.data);
+    console.log(parameters);
+  });
+});
