59985c614fa354a3307507bbfbcc6c4b5cdae769
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-09 15:06:02 +0100

    refactor: move from tsup to bunup

---
+++ bunup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "bunup";
+
+export default defineConfig({
+  clean: true,
+  dts: {
+    inferTypes: true
+  },
+  entry: [
+    "src-new/index.ts"
+  ],
+  format: ["cjs", "esm"],
+  minify: true,
+  outDir: "dist",
+  splitting: false
+});
