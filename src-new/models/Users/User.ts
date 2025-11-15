0c034ecb9ec38f6185a2af6bad6303f25a1b6e11
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-15 07:31:53 +0100

    feat: expose some properties of user with comments

--- src-new/models/Users/User.ts
+++ src-new/models/Users/User.ts
@@ -1,19 +1,42 @@
-import { Authentication } from "../Authentication";
-import { Parameters } from "../Parameters";
-import { Session } from "../Session";
-import { UserParameters } from "../UserParameters";
+import { UserParameters } from "~/models/UserParameters";
+import { Authentication } from "~/models/Authentication";
+import { Parameters } from "~/models/Parameters";
+import { Session } from "~/models/Session";
 
+/**
+ * An abstract PRONOTE authenticated user.
+ * Contains properties that is used globally for every webspaces.
+ */
 export abstract class User {
-  /** @internal */
+  /**
+   * Only the library can instantiate this class.
+   * @internal
+   */
   protected constructor(
-    /** @internal */
-    public readonly user: UserParameters,
-    /** @internal */
+    protected readonly user: UserParameters,
+
+    /**
+     * Everything to call APIs and communicate with PRONOTE.
+     * Should not be used outside the library.
+     *
+     * @internal
+     */
     public readonly session: Session,
-    /** @internal */
+
+    /**
+     * Parameters given to the user at the beginning of the session.
+     * In this, you'll be able to find multiple properties used
+     * to know how the instance is configured.
+     */
     public readonly parameters: Parameters,
-    /** @internal */
-    public readonly authentication: Authentication
+
+    /**
+     * Properties used and given during authentication that could be
+     * helpful for next authentications.
+     *
+     * @internal
+     */
+    private readonly authentication: Authentication
   ) {}
 
   public get username(): string {
@@ -29,14 +52,21 @@ export abstract class User {
   }
 
   public get id(): string {
-    return this.user.ressource.id;
+    return this.user.resource.n;
   }
 
   public get name(): string {
-    return this.user.ressource.name;
+    return this.user.resource.l;
   }
 
   public get kind(): number {
-    return this.user.ressource.kind;
+    return this.user.resource.g;
+  }
+
+  /**
+   * A quick alias for {@link Parameters.navigatorIdentifier | `this.parameters.navigatorIdentifier`}.
+   */
+  public get navigatorIdentifier(): string | null {
+    return this.parameters.navigatorIdentifier;
   }
 }
